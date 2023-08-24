dd3ed5d92679f3c7de18462dca2e4ab441d174e1
Abhinayksingh <89795523+Abhinayksingh@users.noreply.github.com>
2023-08-24 12:36:09 +0530

    project setup

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {app} from "./server.js"
+
+app.listen(3001, ()=>{
+    console.log("server is running at http://localhost:3001")
+})
+
+
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+export const app = express()
+
+app.get("/", (req, res)=>{
+
+    res.status(200)
+    res.json({message:"hello"})
+
+})
+
+
+
+
+
+
+
+
+
